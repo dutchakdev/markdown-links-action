54afecef6507075af200f8d46c899da95f1c6fb4
Roman Dutchak <dutchakdev@gmail.com>
2023-12-15 23:51:06 +0000

    fix: fix addition file patahs

--- src/utils/filesUtils.ts
+++ src/utils/filesUtils.ts
@@ -3,72 +3,82 @@ import path from 'path'
 import { exec } from 'child_process'
 import { promisify } from 'util'
 const readdirAsync = fs.promises.readdir
+import {
+    warn as _warn,
+} from './logUtils'
 
 export interface FileCheckOptions {
-  folderPaths: string[]
-  fileExtension: string
-  maxDepth: number
-  checkModifiedFilesOnly: boolean
-  baseBranch: string
-  additionalFilePaths: string[]
+    folderPaths: string[]
+    fileExtension: string
+    maxDepth: number
+    checkModifiedFilesOnly: boolean
+    baseBranch: string
+    additionalFilePaths: string[]
 }
 
 async function getFiles(
-  dir: string,
-  fileExtension: string,
-  maxDepth: number,
-  currentDepth = 0
+    dir: string,
+    fileExtension: string,
+    maxDepth: number,
+    currentDepth = 0
 ): Promise<string[]> {
-  if (maxDepth >= 0 && currentDepth > maxDepth) {
-    return []
-  }
+    if (maxDepth >= 0 && currentDepth > maxDepth) {
+        return []
+    }
 
-  const dirents = await readdirAsync(dir, { withFileTypes: true })
-  const files = await Promise.all(
-    dirents.map(async dirent => {
-      const res = path.resolve(dir, dirent.name)
-      return dirent.isDirectory()
-        ? getFiles(res, fileExtension, maxDepth, currentDepth + 1)
-        : res
-    })
-  )
+    const dirents = await readdirAsync(dir, { withFileTypes: true })
+    const files = await Promise.all(
+        dirents.map(async dirent => {
+            const res = path.resolve(dir, dirent.name)
+            return dirent.isDirectory()
+                ? getFiles(res, fileExtension, maxDepth, currentDepth + 1)
+                : res
+        })
+    )
 
-  return Array.prototype
-    .concat(...files)
-    .filter(file => file.endsWith(fileExtension))
+    return Array.prototype
+        .concat(...files)
+        .filter(file => file.endsWith(fileExtension))
 }
 
 export async function getModifiedFiles(baseBranch: string): Promise<string[]> {
-  const execAsync = promisify(exec)
-  try {
-    const { stdout } = await execAsync(`git diff --name-only ${baseBranch}`)
-    return stdout.split('\n').filter(line => line)
-  } catch (error) {
-    console.error('Error occurred while getting modified files:', error)
-    return []
-  }
+    const execAsync = promisify(exec)
+    try {
+        const { stdout } = await execAsync(`git diff --name-only ${baseBranch}`)
+        return stdout.split('\n').filter(line => line)
+    } catch (error) {
+        console.error('Error occurred while getting modified files:', error)
+        return []
+    }
 }
 
 async function getFilesToCheck(options: FileCheckOptions): Promise<string[]> {
-  const allFiles = []
+    const allFiles = []
 
-  for (const folderPath of options.folderPaths) {
-    const files = await getFiles(
-      folderPath,
-      options.fileExtension,
-      options.maxDepth
-    )
-    allFiles.push(...files)
-  }
+    for (const folderPath of options.folderPaths) {
+        const files = await getFiles(
+            folderPath,
+            options.fileExtension,
+            options.maxDepth
+        )
+        allFiles.push(...files)
+    }
 
-  allFiles.push(...options.additionalFilePaths)
+    for (const filePath of options.additionalFilePaths) {
+        // check if file exists
+        if (fs.existsSync(filePath)) {
+            allFiles.push(filePath)
+        } else {
+            _warn(`File ${filePath} does not exist.`)
+        }
+    }
 
-  if (options.checkModifiedFilesOnly) {
-    const modifiedFiles = await getModifiedFiles(options.baseBranch)
-    return allFiles.filter(file => modifiedFiles.includes(file))
-  }
+    if (options.checkModifiedFilesOnly) {
+        const modifiedFiles = await getModifiedFiles(options.baseBranch)
+        return allFiles.filter(file => modifiedFiles.includes(file))
+    }
 
-  return allFiles
+    return allFiles
 }
 
 export default getFilesToCheck
